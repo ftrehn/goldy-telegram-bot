85240dfdb07ba07234c0a339dbee3f324d1235a9
c3equalz <dan.kovalev2013@gmail.com>
2026-08-02 13:17:08 +0300

    fix: fixing event type for correct generation

--- src/goldy/domain/common/event.py
+++ src/goldy/domain/common/event.py
@@ -6,21 +6,9 @@ from .event_id import EventId
 
 @dataclass(frozen=True, kw_only=True)
 class Event:
-    event_id: EventId | None = field(default=None, init=False)
-    event_date: datetime | None = field(default=None, init=False)
+    event_id: EventId
+    event_date: datetime
 
     @property
     def event_type(self) -> str:
         return self.__class__.__name__
-
-    def set_event_id(self, event_id: EventId) -> None:
-        if self.event_id:
-            return
-
-        object.__setattr__(self, "event_id", event_id)  # ruff:ignore[unnecessary-dunder-call]
-
-    def set_event_date(self, event_date: datetime) -> None:
-        if self.event_date:
-            return
-
-        object.__setattr__(self, "event_date", event_date)  # ruff:ignore[unnecessary-dunder-call]
